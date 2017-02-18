a25af5a99daa17ee6470c370022438ccdbbc1a05
Simon Østergaard Kristensen <sok@sekoia.dk>
2017-02-18 16:55:54 +0100

    Working on hidden markov model and viterbi

--- Hidden Markov Model.py
+++ Hidden Markov Model.py
@@ -1,15 +1,25 @@
 from Bio import SeqIO
 import math
+import string
 
 observables = {'A':0, 'C':1, 'E':2, 'D':3, 'G':4, 'F':5, 'I':6, 'H':7, 'K':8, 'M':9, 'L':10, 'N':11, 'Q':12, 'P':13, 'S':14, 'R':15, 'T':16, 'W':17, 'V':18, 'Y':19 }
 
+# Our initial propbabilities for the three different states
 init_probs = [0.496359, 0.188095, 0.315546]
+#Our three different states
 states = {'i':0, 'M':1, 'o':2}
+num_of_states = 3
+# An array that converts index to a state
+index_to_states = {0:'i', 1: 'M', 2: 'o'}
 
+# Transition probabiliy. This describes the probabaity of changing state given a state
+# So in general we can see that if we observe a state there is a high probability we will stay in that state.
 trans_probs = [[0.990971, 0.009029, 0.000000],
                [0.023083, 0.953090, 0.023827],
                [0.000000, 0.013759, 0.986241]]
 
+# Emition probabilities describes how our observations relates to our states For the different observations what is the probability of the different states.
+#So we have 20 coloumns - one for each observation. And we have three rows on for each state.
 emit_probs = [[0.043601, 0.011814, 0.053446, 0.065541, 0.049508, 0.049789, 0.054571, 0.024191, 0.055977, 0.035162, 0.103235, 0.045007, 0.029536, 0.048101, 0.075105, 0.059634, 0.068354, 0.016315, 0.067792, 0.043319],
               [0.102010, 0.019360, 0.009680, 0.011914, 0.033507, 0.103500, 0.118392, 0.003723, 0.000745, 0.039464, 0.138496, 0.014147, 0.011914, 0.026806, 0.067014, 0.012658, 0.073716, 0.037230, 0.119136, 0.056590],
               [0.082374, 0.008415, 0.059345, 0.059345, 0.069973, 0.031001, 0.049159, 0.019043, 0.081045, 0.025244, 0.068202, 0.047830, 0.032772, 0.052259, 0.073959, 0.086802, 0.056244, 0.007086, 0.062445, 0.027458]]
@@ -35,19 +45,92 @@ def log_joint_prob(x, z):
         logp = logp + math.log(trans_probs[z[i - 1]][z[i]]) + math.log(emit_probs[z[i]][x[i]])
     return logp
 
+# Return the index of the highet value in list
+def arg_max(l):
+    maxVal, maxIndex = l[0], 0
+    for i in range(1, len(l)):
+        if l[i] > maxVal:
+            maxVal = l[i]
+            maxIndex = i
+    return maxIndex
 
+def log(x):
+    if x == 0:
+        return float("-inf")
+    else:
+        return math.log(x)
+
+# Compute most likely sequence given observations. 
+def viterbi_decoding(obs):
+        w = []
+         #First(0th) column is calcuated based on initial propabilities and emit probabilities
+         # So it is our initial probabilities + the emission probability of our 0th observation.abs
+         # All three rows are calculated as s will count of 3 times while o will stay at 0
+        w.append(["-inf"] * num_of_states) 
+        for st in range(num_of_states):
+            w[0][st] = (log(init_probs[st]) + log(emit_probs[st][obs[0]]))
+        
+        # ω[k][n] = “minus infinity”
+        #     if p(x[n] | k) != 0:
+        #         for j = 1 to K:
+        #             if p(k | j) != 0:
+        #                 ω^[k][n] = max( ω^[k][n], log(p(x[n] | k)) + ω^[ j ][n-1] + log(p( k | j)) )
+
+        #We then initialize the values of the rest of the array
+        for o in range(1, len(obs)):
+            w.append(["-inf"] * num_of_states) # Each iteration we append a new column with num_of_states as amount of rows
+            for s in range(num_of_states):
+                highestPrevVal = float("-inf") # we set a highestPreVal, This needs to be -inf as the log of trans-probs[][] might be as low as -inf
+                #We search we the highestPreVal for the previous column
+                for st in range(num_of_states):
+                    #We go through all probabilities for the states of that column and set it to the highest value
+                    newVal = w[o-1][st] + log(trans_probs[st][s]) 
+                    if(newVal > highestPrevVal):
+                        highestPrevVal = w[o-1][st] + log(trans_probs[st][s]) 
+                #We set the value of the array element [o][s] to emit_probs + the highestPrevVal
+                #we use the emition probability for each state in respect to the state that had the highest probability in the previous column
+                #So basically we take the previous value with the highest probability and add the emission probability for each state.
+                w[o][s] = log(emit_probs[s][obs[o]]) + highestPrevVal
+        
+        #The table for decoding is now created based on our observations
+        # We now backtrack through our table
+        z = len(obs) * [None]
+        z[len(z)-1] = arg_max(w[len(z)-1])
+        for i in range(len(z)-1, 0, -1):
+            for j in range(num_of_states):
+                if w[i-1][j] + log(trans_probs[j][z[i]]) + log(emit_probs[z[i]][obs[i]]) == w[i][z[i]]:
+                    z[i-1] = j
+                    break
+        # z[1..N] = undef
+        # z[N] = arg maxk ω^[k][N]
+        # for n = N-1 to 1:
+        #     z[n] = arg maxk( log p(x[n+1] | z[n+1]) + ω^[k][n] + log p(z[n+1] | k ) )
+        return z, w[-1][z[-1]]
+            
+
+            
+ 
+    
 for recordX in SeqIO.parse ("xval.txt", "fasta"):
     Lx = list(recordX)
 
 for recordZ in SeqIO.parse ("zval.txt", "fasta"):
     Lz = list(recordZ)
 
-print(recordXZ.seq)
-
 Lxx = [observables[c] for c in Lx]
 Lzz = [states[c] for c in Lz]
 
+#viterbi_decoding(Lxx)
 
-#print(joint_prob(Lxx, Lzz))
-#print(log_joint_prob(Lxx,Lzz))
+# Compute Viterbi decoding and its loglikelihood
+z, logpz = viterbi_decoding(Lxx)
+
+# Output the results
+print "Viterbi path z:", string.join([index_to_states[c] for c in Lzz]) 
+print "loglikelihood of z:", logpz
+print "Joint loglikelihood of (x,z):", log_joint_prob(Lxx, Lzz)
+
+
+print(joint_prob(Lxx, Lzz))
+print(log_joint_prob(Lxx,Lzz))
 
